d92be9fe35ef473a927557d96fa83dfb2731fbf3
Sulman Baig <sulman@hey.com>
2023-03-08 16:08:13 +0500

    using useFocus and keyboardEvent

--- src/NewItemForm.tsx
+++ src/NewItemForm.tsx
@@ -4,6 +4,7 @@ import {
   NewItemButton,
   NewItemInput
 } from "./styles"
+import { useFocus } from "./utils/useFocus"
 
 type NewItemFormProps = {
   onAdd(text: string): void
@@ -11,12 +12,21 @@ type NewItemFormProps = {
 
 export const NewItemForm = ({ onAdd }: NewItemFormProps) => {
   const [text, setText] = useState("")
+  const inputRef = useFocus()
+
+  const handleAddText = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      onAdd(text)
+    }
+  }
   
   return (
     <NewItemFormContainer>
       <NewItemInput
+        ref={inputRef}
         value={text}
         onChange={e => setText(e.target.value)}
+        onKeyPress={handleAddText}
       />
       <NewItemButton onClick={() => onAdd(text)}>
         Create
